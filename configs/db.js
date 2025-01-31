63338eb5505724404cb791ef9b2d99047d356341
yban <laubana@gmail.com>
2025-01-31 09:35:08 -0800

    minor changes

--- configs/db.js
+++ configs/db.js
@@ -8,12 +8,10 @@ export default async () => {
   try {
     mongoose.set("strictQuery", false);
 
-    if (process.env.DB_URI) {
-      await mongoose.connect(process.env.DB_URI);
-    } else {
-      console.error("DB not connected.");
-    }
+    await mongoose.connect(process.env.DB_URI);
   } catch (error) {
     console.error(error);
+
+    console.error("Failed to connect to DB 🚨");
   }
 };
